da14f34ddb34834d2315a780ad5ee11cfdc3ec1e
Miraj Hamid <miraj.hamid@ebuilder.com>
2019-04-11 12:05:28 +0530

    Update App comment

--- src/main/java/com/example/demo/App.java
+++ src/main/java/com/example/demo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello eBuilder!" );
+        System.out.println( "Hello eBuilder! :D" );
     }
 }
